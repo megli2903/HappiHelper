6474f27c031bc76dd87bfa4b207e26abf0c6f1b9
Meg Li <mli3023@gmail.com>
2023-10-08 14:24:53 -0500

    update

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import main  # Import your Python script (main.py)
 from main import Trainer
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # Call a function from your main.py script to get data
-    data = main.get_data()  # Replace with your actual function
-    return render_template('startjourney.html', data=data)
+# @app.route('/')
+# def home():
+#     # Call a function from your main.py script to get data
+#     data = main.get_data()  # Replace with your actual function
+#     return render_template('startjourney.html', data=data)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = Trainer(social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None) 
-    return render_template('pathselect.html', user=user)
+    user_name = None  # Initialize user_name to None
 
+    if request.method == 'POST':
+        # If the form is submitted, get the user's name from the form
+        user_name = request.form.get('user_name')
+
+    return render_template('trainerinfo.html', user_name=user_name)
+
+
+# @app.route("/")
+# def home():
+#     return render_template('trainerinfo.html')
 # @app.route('/social')
 # def social():
 #     user = Trainer(social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None)   # You might want to replace this with the actual user data
