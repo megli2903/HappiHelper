5b246648679018ce3ad4aa7c08082a1b13e87a0d
Nathan Joseph <nathanjj625@gmail.com>
2023-10-08 13:50:40 -0500

    Finished Backend for Happi Helper

--- main.py
+++ main.py
@@ -17,60 +17,76 @@
     # "ENTJ": ["Commander", "Leadership", "Ambitious", "Strategic"]
     # }
 # pokedex = {}
-evolution_lines = {"Bulbasaur":"Ivysaur", "Ivysaur":"Venusaur",
-    "Charmander":"Charmeleon","Charmeleon":"Charizard",
-    "Squirtle":"Wartortle", "Wartortle":"Blastoise",
-    "Caterpie": "Metapod", "Metapod":"Butterfree",
-    "Weedle":"Kakuna", "Kakuna":"Beedrill",
-    "Pidgey":"Pidgeotto", "Pidgeotto":"Pidgeot",
-    "Rattata":"Raticate",
-    "Spearow":"Fearow",
-    "Ekans":"Arbok",
-    "Pikachu":"Raichu",
-    "Sandshrew":"Sandslash",
-    "Nidoran♀":"Nidorina", "Nidorina":"Nidoqueen", #special pokemon
-    "Nidoran♂":"Nidorino", "Nidorino":"Nidoking", #special pokemon
-    "Clefairy":"Clefable",
-    "Vulpix":"Ninetales",
-    "Jigglypuff":"Wigglytuff",
-    "Zubat":"Golbat",
-    "Oddish":"Gloom", "Gloom":"Vileplume",
-    "Paras":"Parasect",
-    "Venonat":"Venomoth",
-    "Diglett":"Dugtrio",
-    "Meowth":"Persian",
-    "Psyduck":"Golduck",
-    "Mankey":"Primeape",
-    "Growlithe":"Arcanine",
-    "Poliwag":"Poliwhirl", "Poliwhirl":"Poliwrath",
-    "Abra":"Kadabra", "Kadabra":"Alakazam",
-    "Machop":"Machoke", "Machoke":"Machamp",
-    "Bellsprout":"Weepinbell", "Weepinbell":"Victreebel",
-    "Tentacool":"Tentacruel",
-    "Geodude":"Graveler", "Graveler":"Golem",
-    "Ponyta":"Rapidash",
-    "Slowpoke":"Slowbro",
-    "Magnemite":"Magneton",
-    "Doduo":"Dodrio",
-    "Seel":"Dewgong",
-    "Grimer":"Muk",
-    "Shellder":"Cloyster",
-    "Gastly":"Haunter","Haunter":"Gengar",
-    "Drowzee":"Hypno",
-    "Krabby":"Kingler",
-    "Voltorb":"Electrode",
-    "Exeggcute":"Exeggutor",
-    "Cubone":"Marowak",
-    "Koffing":"Weezing",
-    "Rhyhorn":"Rhydon",
-    "Horsea":"Seadra",
-    "Goldeen":"Seaking",
-    "Staryu":"Starmie",
-    "Magikarp":"Gyarados",
-    #"Eevee", "Vaporeon", "Jolteon", "Flareon",
-    "Omanyte":"Omastar",
-    "Kabuto":"Kabutops",
-    "Dratini":"Dragonair", "Dragonair":"Dragonite"
+evolution_lines = {
+    "bulbasaur": "ivysaur",
+    "ivysaur": "venusaur",
+    "charmander": "charmeleon",
+    "charmeleon": "charizard",
+    "squirtle": "wartortle",
+    "wartortle": "blastoise",
+    "caterpie": "metapod",
+    "metapod": "butterfree",
+    "weedle": "kakuna",
+    "kakuna": "beedrill",
+    "pidgey": "pidgeotto",
+    "pidgeotto": "pidgeot",
+    "rattata": "raticate",
+    "spearow": "fearow",
+    "ekans": "arbok",
+    "pikachu": "raichu",
+    "sandshrew": "sandslash",
+    "nidoran♀": "nidorina",
+    "nidorina": "nidoqueen",
+    "nidoran♂": "nidorino",
+    "nidorino": "nidoking",
+    "clefairy": "clefable",
+    "vulpix": "ninetales",
+    "jigglypuff": "wigglytuff",
+    "zubat": "golbat",
+    "oddish": "gloom",
+    "gloom": "vileplume",
+    "paras": "parasect",
+    "venonat": "venomoth",
+    "diglett": "dugtrio",
+    "meowth": "persian",
+    "psyduck": "golduck",
+    "mankey": "primeape",
+    "growlithe": "arcanine",
+    "poliwag": "poliwhirl",
+    "poliwhirl": "poliwrath",
+    "abra": "kadabra",
+    "kadabra": "alakazam",
+    "machop": "machoke",
+    "machoke": "machamp",
+    "bellsprout": "weepinbell",
+    "weepinbell": "victreebel",
+    "tentacool": "tentacruel",
+    "geodude": "graveler",
+    "graveler": "golem",
+    "ponyta": "rapidash",
+    "slowpoke": "slowbro",
+    "magnemite": "magneton",
+    "doduo": "dodrio",
+    "seel": "dewgong",
+    "grimer": "muk",
+    "shellder": "cloyster",
+    "gastly": "haunter",
+    "haunter": "gengar",
+    "drowzee": "hypno",
+    "krabby": "kingler",
+    "voltorb": "electrode",
+    "exeggcute": "exeggutor",
+    "cubone": "marowak",
+    "koffing": "weezing",
+    "rhyhorn": "rhydon",
+    "horsea": "seadra",
+    "goldeen": "seaking",
+    "staryu": "starmie",
+    "magikarp": "gyarados",
+    "omanyte": "omastar",
+    "kabuto": "kabutops",
+    "dratini": "dragonair",
+    "dragonair": "dragonite"
 }
 
 
@@ -105,28 +121,61 @@ task_list = {
 
 #Social path pokemon and tasks
 
-farfetchd = Pokemon("Farfetch'd")
-jigglypuff = Pokemon("Jigglypuff")
-clefairy = Pokemon("Clefairy")
+farfetchd = Pokemon("farfetch'd")
+jigglypuff = Pokemon("jigglypuff")
+clefairy = Pokemon("clefairy")
 
 fartask = Task(farfetchd, "Connect with loved ones", "Initiate social interactions regularly to maintain connections with friends and loved ones.")
-jigtask = Task(jigglypuff, "Attend social event", "Attend social events, gatherings, or group activities to recharge your energy")
+jigtask = Task(jigglypuff, "Attend social event", "Attend social events, gatherings, or group activities to recharge your energy.")
 cleftask = Task(clefairy, "Team Building", "Engage in team-oriented projects or collaborative activities to satisfy your need for interaction.")
 
 #Introvert path pokemon and tasks
 
-abra = Pokemon("Abra")
-cubone = Pokemon("Cubone")
-kabuto = Pokemon("Kabuto")
+abra = Pokemon("abra")
+cubone = Pokemon("cubone")
+kabuto = Pokemon("kabuto")
 
-abratask = Task("Abra", "Allocate me-time", "Allocate regular me-time for solitude and self-reflection.")
-cubtask = Task("Cubone", "Create cozy atmosphere", "Create a quiet and cozy space at home for relaxation and personal activities.")
-kabtask = Task("Kabuto", "Explore your hobbies", "Dedicate time to your favorite solitary hobbies, such as reading, writing, or art.")
+abratask = Task(abra, "Allocate me-time", "Allocate regular me-time for solitude and self-reflection.")
+cubtask = Task(cubone, "Create cozy atmosphere", "Create a quiet and cozy space at home for relaxation and personal activities.")
+kabtask = Task(kabuto, "Explore your hobbies", "Dedicate time to your favorite solitary hobbies, such as reading, writing, or art.")
+
+# Perceiving path pokemon and tasks
+
+zubat = Pokemon("zubat")
+bellsprout = Pokemon("bellsprout")
+machop = Pokemon("machop")
+
+zubtask = Task(zubat, "Explore new places", "Embrace spontaneity by trying new activities or exploring new places.")
+beltask = Task(bellsprout, "Adapt to change", "Stay adaptable and open to change in your daily routine.")
+mactask = Task(machop, "Pursuit of interest", "Dedicate time to pursue multiple interests or hobbies without rigid schedules")
+
+# Judging path pokemon and tasks
+
+gastly = Pokemon("gastly")
+horsea = Pokemon("horsea")
+vulpix = Pokemon("vulpix")
+
+gastask = Task(gastly, "Structured Environment", "Maintain a well-organized and structured environment to reduce stress.")
+hortask = Task(horsea, "Schedule Yourself", "Create daily to-do lists or schedules to manage your time effectively.")
+vultask = Task(vulpix, "Goal Setting", "Set specific goals and track your progress towards achieving them.")
+
+
+#all tasks list
 
 task_list = {
 "farfetch'd": fartask,
 "jigglypuff": jigtask,
-"clefairy": cleftask
+"clefairy": cleftask,
+"abra": abratask,
+"cubone": cubtask,
+"kabuto": kabtask,
+"bellsprout": beltask,
+"zubat": zubtask,
+"machop": mactask,
+"gastly": gastask,
+"horsea": hortask,
+"vulpix": vultask
+
 }
 
 
@@ -152,62 +201,38 @@ def welcome():
     else:
         trainer_type.plan = "planning"
     return trainer_type
-
-
 user = welcome()
-
-def professor_lab():
-    print("Hello again " + user.name + "! It is great to see you begin your journey within Sassyland. Today is an exciting day! You will be meeting your first pokemon.")
-    starter_choice = input("Please choose your starter! ").lower()
-    starter_pokemon = Pokemon(starter_choice)
-    if starter_choice == "bulbusaur":
-        starter_pokemon.evolution = "ivysaur"
-    elif starter_choice == "squirtle":
-        starter_pokemon.evolution = "ivysaur"
-    else:
-        starter_pokemon.evolution = "charmeleon"
-
-    pokemon_list = [starter_pokemon]
-    pokemon_party = PokemonParty(pokemon_list)
-    user.party = pokemon_party
-    print("Congratulations on your " + starter_pokemon.name + ", " + user.name + "! It seems to be really attached to you! This " + starter_pokemon.name + " will be your first buddy to explore the region.")
-    path_choice = input("Based on our extensive research on your personality, we have determined the routes that will prove most exciting! Please select one of the following: " + user.social + " or " + user.plan + "! ").lower()
-    if path_choice == user.social or path_choice == user.plan:
-        print("Great choice!")
-        user.currentpath = path_choice
-
-professor_lab()
-
-
-# print(user.name, user.party, user.social, user.obs, user.thinking, user.plan)
-
 def evolution():
     evolvable_pokemon = []
     evolvable_pokemon_names = []
     for pokemon in user.party.party:
-        if evolution_lines.has_key(pokemon):
+        print (pokemon.name)
+        if pokemon.name.lower() in evolution_lines.keys():
             evolvable_pokemon.append(pokemon)
             evolvable_pokemon_names.append(pokemon.name)
-    if evolvable_pokemon_names.len() == 0:
+    if len(evolvable_pokemon_names) == 0:
         print("Sorry, all your pokemon are fully evolved")
-    toevolve = input("Please select a pokemon to evolve: " + evolvable_pokemon_names + ": ")
-    if toevolve.lower() in       
-    print("insert evolution print here")
-
-
+        return
+    print("Please select the slot of the pokemon you would like to evolve!")
+    evo_idx = input(evolvable_pokemon_names) # this needs to take in an integer value
+    idx = int(evo_idx)
+    if idx < len(evolvable_pokemon_names):
+        prevolution = evolvable_pokemon[idx].name
+        evolvable_pokemon[idx].name = evolution_lines.get(evolvable_pokemon[idx].name)
+        print("Congratulations! Your " + prevolution + " has evolved into a " + evolvable_pokemon[idx].name + "!")
 
 def social_path():
     charlotte_party = PokemonParty()
-    farfetchd = Pokemon("Farfetch'd")
-    jigglypuff = Pokemon("Jigglypuff")
-    clefairy = Pokemon("Clefairy")
+    farfetchd = Pokemon("farfetch'd")
+    jigglypuff = Pokemon("jigglypuff")
+    clefairy = Pokemon("clefairy")
     charlotte_party.party = [farfetchd, jigglypuff, clefairy]
     charlotte_reindeer = Trainer(social="social", name="Charlotte Reindeer", party=charlotte_party)
     print("Hey trainer! My name is " + charlotte_reindeer.name + " and I will be taking you on the " + charlotte_reindeer.social + " path! To get started on your journey, we will be providing you some social tasks.")
     social_task = input("Please select one of the following paths: " + fartask.pokemon.name + ": " + fartask.title + " OR " + jigtask.pokemon.name + ": " + jigtask.title + " OR " + cleftask.pokemon.name + ": " + cleftask.title + "? ").lower()
     if (social_task == fartask.pokemon.name.lower() or social_task == jigtask.pokemon.name.lower() or social_task == cleftask.pokemon.name.lower()):
         thetask = task_list.get(social_task)
-        finishedtask = input("Hiya trainer! My name is: " + thetask.pokemon.name + " and your task today is: " + thetask.desc + ". Have you completed this task?").lower()
+        finishedtask = input("Hiya trainer! My name is: " + thetask.pokemon.name + " and your task today is: " + thetask.desc + " Have you completed this task? ").lower()
         if (finishedtask == "yes"):
             choosecatch = input("Congratulations! You are one step closer to becoming champion of the Sassyland region! I am very inspired by what you have done. Would I be able to join your team? If not, the least I can do is help you evolve your pokemon: ").lower()
             if (choosecatch == "evolve"):
@@ -219,7 +244,116 @@ def social_path():
             print("It was nice to meet you! Truly sorry to see you go :(")
     else: 
         print("Did not enter the task")
+    path_choice()
 
+def introspect_path():
+    meg_party = PokemonParty()
+    abra = Pokemon("abra")
+    kabuto = Pokemon("kabuto")
+    cubone = Pokemon("cubone")
+    meg_party.party = [abra, kabuto, cubone]
+    meg_liger = Trainer(social="introspect", name="Meg Liger", party=meg_party)
+    print("Hey trainer... I'm " + meg_liger.name + " and I will be taking you on the " + meg_liger.social + " path. This will be a great space to take some time dedicated to yourself.")
+    social_task = input("Please select one of the following paths: " + abratask.pokemon.name + ": " + abratask.title + " OR " + cubtask.pokemon.name + ": " + cubtask.title + " OR " + kabtask.pokemon.name + ": " + kabtask.title + "? ").lower()
+    if (social_task == abratask.pokemon.name.lower() or social_task == cubtask.pokemon.name.lower() or social_task == kabtask.pokemon.name.lower()):
+        thetask = task_list.get(social_task)
+        finishedtask = input("Hi I'm " + thetask.pokemon.name + " and your task today is: " + thetask.desc + " Have you completed this task? ").lower()
+        if (finishedtask == "yes"):
+            choosecatch = input("Congratulations! You are one step closer to becoming champion of the Sassyland region! I am very inspired by what you have done. Can I join you? If not, I'll help you evolve: ").lower()
+            if (choosecatch == "evolve"):
+                evolution()
+            else:
+                user.party.party.append(thetask.pokemon)
+                print("Congratulations! " + thetask.pokemon.name + " has joined your party!")
+        else:
+            print("It was nice to meet you! Truly sorry to see you go :(")
+    else: 
+        print("Did not enter the task")    
+    path_choice()
 
+def perceiving_path():
+    nate_party = PokemonParty()
+    machop = Pokemon("machop")
+    zubat = Pokemon("zubat")
+    bellsprout = Pokemon("bellsprout")
+    nate_party.party = [machop, zubat, bellsprout]
+    nate_jabiru = Trainer(plan="spontaneous", name="Nate Jabiru", party=nate_party)
+    print("Didn't see you coming! I'm " + nate_jabiru.name + " and I will be taking you on the " + nate_jabiru.plan + " path! You will be able to spend time really going on a true adventure.")
+    social_task = input("Please select one of the following paths: " + zubtask.pokemon.name + ": " + zubtask.title + " OR " + mactask.pokemon.name + ": " + mactask.title + " OR " + beltask.pokemon.name + ": " + beltask.title + "? ").lower()
+    if (social_task == zubtask.pokemon.name.lower() or social_task == mactask.pokemon.name.lower() or social_task == beltask.pokemon.name.lower()):
+        thetask = task_list.get(social_task)
+        finishedtask = input("Hi I'm " + thetask.pokemon.name + " and your task today is: " + thetask.desc + " Have you completed this task? ").lower()
+        if (finishedtask == "yes"):
+            choosecatch = input("Congrats bestie! You are one step closer to becoming champion of the Sassyland region! I am very inspired by what you have done. I know it's last minute, but I'd love to join you! Can I join you? Otherwise I'll help a buddy of yours evolve").lower()
+            if (choosecatch == "evolve"):
+                evolution()
+            else:
+                user.party.party.append(thetask.pokemon)
+                print("Congratulations! " + thetask.pokemon.name + " has joined your party!")
+        else:
+            print("It was nice to meet you! Truly sorry to see you go :(")
+    else: 
+        print("Did not enter the task")    
+    path_choice()
 
-social_path()
+def judging_path():
+    gem_party = PokemonParty()
+    vulpix = Pokemon("vulpix")
+    gastly = Pokemon("gastly")
+    horsea = Pokemon("horsea")
+    gem_party.party = [vulpix, gastly, horsea]
+    gem_gemmy = Trainer(plan="planning", name="Gem of Ordinance", party=gem_party)
+    print("I was expecting you, young trainer. I'm " + gem_gemmy.name + " and I will be taking you on the " + gem_gemmy.plan + " path! You will be able to plan for what's in store.")
+    social_task = input("Please select one of the following paths: " + vultask.pokemon.name + ": " + vultask.title + " OR " + gastask.pokemon.name + ": " + gastask.title + " OR " + hortask.pokemon.name + ": " + hortask.title + "? ").lower()
+    if (social_task == hortask.pokemon.name.lower() or social_task == vultask.pokemon.name.lower() or social_task == gastask.pokemon.name.lower()):
+        thetask = task_list.get(social_task)
+        finishedtask = input("Hi I'm " + thetask.pokemon.name + " and your task today is: " + thetask.desc + " Have you completed this task? ").lower()
+        if (finishedtask == "yes"):
+            choosecatch = input("Congrats bestie! You are one step closer to becoming champion of the Sassyland region! I am very inspired by what you have done. I know it's last minute, but I'd love to join you! Can I join you? Otherwise I'll help a buddy of yours evolve").lower()
+            if (choosecatch == "evolve"):
+                evolution()
+            else:
+                user.party.party.append(thetask.pokemon)
+                print("Congratulations! " + thetask.pokemon.name + " has joined your party!")
+        else:
+            print("It was nice to meet you! Truly sorry to see you go :(")
+    else: 
+        print("Did not enter the task")    
+    path_choice()
+
+def professor_lab():
+    print("Hello again " + user.name + "! It is great to see you begin your journey within Sassyland. Today is an exciting day! You will be meeting your first pokemon.")
+    starter_choice = input("Please choose your starter! ").lower()
+    starter_pokemon = Pokemon(starter_choice)
+    if starter_choice == "bulbasaur":
+        starter_pokemon.evolution = "ivysaur"
+    elif starter_choice == "squirtle":
+        starter_pokemon.evolution = "wartortle"
+    else:
+        starter_pokemon.evolution = "charmeleon"
+
+    pokemon_list = [starter_pokemon]
+    pokemon_party = PokemonParty(pokemon_list)
+    user.party = pokemon_party
+    print("Congratulations on your " + starter_pokemon.name + ", " + user.name + "! It seems to be really attached to you! This " + starter_pokemon.name + " will be your first buddy to explore the region.")
+
+def path_choice():
+    path_choice = input("Based on our extensive research on your personality, we have determined the routes that will prove most exciting! Please select one of the following: " + user.social + " or " + user.plan + "! ").lower()
+    if path_choice == "introspection":
+        introspect_path()
+    elif path_choice == "social":
+        social_path()
+    elif path_choice == "spontaneous":
+        perceiving_path()
+    elif path_choice == "planning":
+        judging_path()
+    else:
+        print("Come back another time! We look forward to seeing you again in the future.")
+
+professor_lab()
+path_choice()
+
+# print(user.name, user.party, user.social, user.obs, user.thinking, user.plan)
+
+# for pokemon in user.party.party:
+#     print(pokemon.name)
