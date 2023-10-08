2ffbbb4b38521973d5875a1f8196e5c4c8f2c7c0
Nathan Joseph <nathanjj625@gmail.com>
2023-10-08 10:13:07 -0500

    Created starter and intro

--- main.py
+++ main.py
@@ -18,12 +18,18 @@
     # }
 # pokedex = {}
 
+
+
+
 class Trainer:
-    def __init__(self, social=None, name=None, plan=None, party=None):
-        self.social = social
+    def __init__(self, social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None):
+        self.social = social # E or I
+        self.thinking = thinking
+        self.obs = obs
         self.name = name
         self.plan = plan
-        self.party = party
+        self.party = party #pokemon party -> each catch appends
+        self.currentpath = currentpath #path user is currently on
 
 class Pokemon:
     def __init__(self, name=None, evolution=None):
@@ -34,17 +40,30 @@ class PokemonParty:
     def __init__(self, party=None):
         self.party = party
 
+venusaur = Pokemon("Venusaur", None)
+ivysaur = Pokemon("Ivysaur", venusaur)
+bulbusaur = Pokemon("Bulbusaur", ivysaur)
+
+
 
 def welcome():
     nameask = input("Happi you are here! Please enter your name here: ")
     trainer_type = Trainer()
     trainer_type.name = nameask
-    personal = input("Hello " + nameask + "! My name is Professor Banerjee, and I will be taking you to the region of Sassyland. Select your trainer type: ")
-    if "E" in personal:
+    personal = input("Hello " + nameask + "! My name is Professor Banerjee, and I will be taking you to the region of Sassyland. Select your trainer type: ").upper()
+    if personal[0] == "E":
         trainer_type.social = "social"
     else:
-        trainer_type.social = "introspect" 
-    if "P" in personal:
+        trainer_type.social = "introspection"
+    if personal[1] == "N":
+        trainer_type.obs = "intuition"
+    else:
+        trainer_type.obs = "observation"
+    if personal[2] == "F":
+        trainer_type.thinking = "feeling"
+    else:
+        trainer_type.thinking = "thought"
+    if personal[3] == "P":
         trainer_type.plan = "spontaneous"
     else:
         trainer_type.plan = "planning"
@@ -71,7 +90,18 @@ def professor_lab():
     path_choice = input("Based on our extensive research on your personality, we have determined the routes that will prove most exciting! Please select one of the following: " + user.social + " or " + user.plan + "! ").lower()
     if path_choice == user.social or path_choice == user.plan:
         print("Great choice to choose the " + path_choice + " path! A thrilling journey of resilience with " + starter_choice + " awaits!")
+        user.currentpath = path_choice
 
 professor_lab()
 
-print(user.name, user.party, user.plan, user.social)
+print(user.name, user.party, user.social, user.obs, user.thinking, user.plan)
+
+def social_path():
+    charlotte_party = PokemonParty()
+    farfetchd = Pokemon("Farfetch'd", None)
+    jigglypuff = Pokemon("Jigglypuff", "Wigglytuff")
+    clefairy = Pokemon("Clefairy", "Clefable")
+    charlotte_party.append(farfetchd, jigglypuff, clefairy)
+    charlotte_reindeer = Trainer(social="social", name="Charlotte Reindeer", party=charlotte_party)
+    print("Hey trainer! My name is " + charlotte_reindeer.name + " and I will be taking you on the" + charlotte_reindeer.social + "path! To get started on your journey, we will be providing you some social tasks.")
+
