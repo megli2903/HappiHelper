54eb9da8b240caedfdb2bed25bcb7c8f7418949f
Meg Li <mli3023@gmail.com>
2023-10-08 11:17:55 -0500

    Log In Test

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginMessage = document.getElementById("loginMessage");
     const signupMessage = document.getElementById("signupMessage");
 
+    // Simulated user database (for demonstration purposes)
+    const userDatabase = {
+        "sasetrainer": "sasehack123", // Adding a predefined user
+    };
+
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
-        // Login logic (replace with your authentication logic)
-        if (username === "user" && password === "password") {
+        // Simulated login logic (replace with your authentication logic)
+        if (userDatabase[username] && userDatabase[username] === password) {
             loginMessage.textContent = "Login successful!";
             loginMessage.style.color = "green";
         } else {
@@ -25,32 +30,20 @@ document.addEventListener("DOMContentLoaded", function () {
         const signupPassword = document.getElementById("signupPassword").value;
         const confirmPassword = document.getElementById("confirmPassword").value;
 
-        // Signup logic (replace with your registration logic)
-        if (signupPassword !== confirmPassword) {
+        // Simulated signup logic (for demonstration purposes)
+        if (userDatabase[signupUsername]) {
+            signupMessage.textContent = "Username already exists. Please choose a different one.";
+            signupMessage.style.color = "red";
+        } else if (signupPassword !== confirmPassword) {
             signupMessage.textContent = "Password and confirm password do not match.";
             signupMessage.style.color = "red";
         } else {
-            // Here, you can add your user registration code.
-            // For example, you can call a function like registerUser(signupUsername, signupPassword).
-            // Make sure to replace this with your actual registration logic.
-            const registrationResult = registerUser(signupUsername, signupPassword);
-            
-            // Display the registration result
-            signupMessage.textContent = registrationResult.message;
-            signupMessage.style.color = registrationResult.success ? "green" : "red";
+            // Add the new user to the simulated database (replace with actual registration logic)
+            userDatabase[signupUsername] = signupPassword;
+
+            // Registration successful
+            signupMessage.textContent = "Registration successful!";
+            signupMessage.style.color = "green";
         }
     });
-    
-    // Function to register a user (replace with your registration logic)
-    function registerUser(username, password) {
-        // Replace this with your actual registration logic.
-        // You can use the example from previous responses or integrate it with a database.
-        
-        // For now, let's assume a successful registration.
-        // In practice, you should add error handling and database integration here.
-        return {
-            success: true,
-            message: "Registration successful!",
-        };
-    }
 });
