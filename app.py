c02954ab5baa98e48902fb5049914ae1a06d9066
Meg Li <mli3023@gmail.com>
2023-10-08 12:59:42 -0500

    app.py start

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import main  # Import your Python script (main.py)
+from main import Trainer
 
 app = Flask(__name__)
 
@@ -8,6 +9,23 @@ def home():
     # Call a function from your main.py script to get data
     data = main.get_data()  # Replace with your actual function
     return render_template('startjourney.html', data=data)
+
+@app.route('/')
+def index():
+    user = Trainer(social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None) 
+    return render_template('pathselect.html', user=user)
+
+# @app.route('/social')
+# def social():
+#     user = Trainer(social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None)   # You might want to replace this with the actual user data
+#     # Modify this function to return relevant data for the social path
+#     return render_template('social.html', user=user)
+
+# @app.route('/spontaneous')
+# def spontaneous():
+#     user = Trainer(social=None, name=None, thinking=None, obs=None, plan=None, currentpath=None, party=None)   # You might want to replace this with the actual user data
+#     # Modify this function to return relevant data for the spontaneous path
+#     return render_template('spontaneous.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
